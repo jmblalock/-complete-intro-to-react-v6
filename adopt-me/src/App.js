8d35b351bf1c66533185962f4170bb72e3642c28
jmblalock <justin.m.blalock@gmail.com>
2021-03-11 16:37:22 -0600

    09-managing-state-in-class-components

--- adopt-me/src/App.js
+++ adopt-me/src/App.js
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Link } from "react-router-dom";
 import SearchParams from "./SearchParams";
 import Details from "./Details";
 
