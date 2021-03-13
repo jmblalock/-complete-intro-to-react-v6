94ee06f7f35db7e7ff2d5ab474f6f0bf9195d539
jmblalock <justin.m.blalock@gmail.com>
2021-03-12 19:12:17 -0600

    testing

--- adopt-me/src/Carousel.js
+++ adopt-me/src/Carousel.js
@@ -21,13 +21,14 @@ class Carousel extends Component {
 
     return (
       <div className="carousel">
-        <img src={images[active]} alt="animal" />
+        <img src={images[active]} data-testid="hero" alt="animal" />
         <div className="carousel-smaller">
           {images.map((photo, index) => (
             <img
               key={photo}
               src={photo}
               data-index={index}
+              data-testid={`thumbnail${index}`}
               onClick={this.handleIndexClick}
               className={index === active ? "active" : ""}
               alt="animal thumbnail"
